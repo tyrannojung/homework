8ee475268428452ece1245d80b94aea425d554a1
jungdawoon <tyranno2020@gmail.com>
2023-09-14 16:22:16 +0900

    first commit

---
+++ contracts/Add.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: onther
+pragma solidity 0.8.19;
+
+contract Name {
+    string public name = "Onther";
+}
+
+function add(uint x, uint y) pure returns (uint){
+    return x + y;
+}
---
+++ contracts/ImportContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: onther
+pragma solidity ^0.8.19;
+
+import "./Add.sol";
+
+contract ImportContract {
+    Name public viewName = new Name();
+
+    function viewNameFunction()  public view returns (string memory){
+        return viewName.name();
+    }
+
+    function addFunction(uint x, uint y)  pure public returns (uint){
+        return add(x, y);
+    }
+}
