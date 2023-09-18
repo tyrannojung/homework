84a35e09408e4549bb3965c7137c814243936781
Dawoon Jung <tyranno2020@gmail.com>
2023-09-18 22:28:14 +0900

    Create TrafficLight.sol

---
+++ contracts/TrafficLight.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract TrafficLight {
+    enum LightState { Green, Red, Yellow }
+
+    LightState public currentLight;
+
+    constructor() {
+        currentLight = LightState.Red;
+    }
+
+    function changeLight(LightState _newState) public {
+        require(_newState != currentLight, "New state");
+        currentLight = _newState;
+    }
+}
