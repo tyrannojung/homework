1b22473e88967fca6381a5cd5f9ac97fcd507056
Dawoon Jung <tyranno2020@gmail.com>
2023-09-18 22:50:57 +0900

    Create HomworkContract.sol

---
+++ contracts/HomworkContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract HomworkContract {
+    //uint256 타입의 변수 선언
+    uint256 public a;
+    
+    //view = read-only & 내-외부 호출이 가능 상속 호출이 가능한 접근 제어자 = view
+    function createContract() public view returns (uint256) {
+        return a;
+    }
+}
