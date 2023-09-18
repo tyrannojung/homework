a120cc3cf5223e650c81bb7d7e278101367b4625
Dawoon Jung <tyranno2020@gmail.com>
2023-09-18 23:31:56 +0900

    Create HomworkArray.sol

---
+++ contracts/HomworkArray.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract HomworkArray {
+
+    // 상태 변수 선언
+    uint[] public dynamic;
+
+    // 함수
+    function setDynamic(uint size) public {
+        // 배열을 생성
+        dynamic = new uint[](size);
+
+        // 배열에 값을 설정
+        for (uint i = 0; i < size; i++) {
+            dynamic[i] = i;
+        }
+    }
+
+    function getDynamic() public view returns (uint[] memory) {
+        // 배열을 반환
+        return dynamic;
+    }
+}
