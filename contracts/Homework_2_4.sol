4e6931a94c6d75598bdefa194111f678acb83500
Dawoon Jung <tyranno2020@gmail.com>
2023-09-25 22:56:39 +0900

    Create Homework_2_4.sol

---
+++ contracts/Homework_2_4.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title Tranfer, send 함수 만들기
+/// @author dawoon jung
+/// @notice trans & send function
+contract HomeworkTransfer {
+
+    /// @dev trans 함수
+    /// @param _to the toAddress
+    /// @param _amount the amount
+    function trans(address payable _to, uint256 _amount) payable public {
+        _to.transfer(_amount);
+    }
+
+    /// @dev send 함수
+    /// @param _to the toAddress
+    /// @param _amount amount
+    function send(address payable _to, uint256 _amount) payable public returns (bool _value) {
+        _value = _to.send(_amount);
+    }
+}
