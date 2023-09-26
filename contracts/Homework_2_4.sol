7cd00b6fdf6ad5947eab28abef6c0104cfe5b375
jungdawoon <tyranno2020@gmail.com>
2023-09-26 09:24:56 +0900

    style guid change

--- contracts/Homework_2_4.sol
+++ contracts/Homework_2_4.sol
@@ -9,14 +9,14 @@ contract HomeworkTransfer {
     /// @dev trans 함수
     /// @param _to the toAddress
     /// @param _amount the amount
-    function trans(address payable _to, uint256 _amount) payable public {
+    function trans(address payable _to, uint256 _amount) public payable {
         _to.transfer(_amount);
     }
 
     /// @dev send 함수
     /// @param _to the toAddress
     /// @param _amount amount
-    function send(address payable _to, uint256 _amount) payable public returns (bool _value) {
+    function send(address payable _to, uint256 _amount) public payable returns (bool _value) {
         _value = _to.send(_amount);
     }
 }
